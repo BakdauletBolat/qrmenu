5efe92f65f314f3865285ae52f9a29ebcaa67d0d
Бакдаулет Болат <bakdauletbolat@Air-Bakdaulet.lan>
2024-02-13 20:42:03 +0600

    bit update

--- qr-front/src/plugins/axios-instance.ts
+++ qr-front/src/plugins/axios-instance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const instance = axios.create({
-    baseURL: 'http://back.easymenu.kz/api',
+    baseURL: 'https://back.easymenu.kz/api',
     timeout: 10000,
   });
