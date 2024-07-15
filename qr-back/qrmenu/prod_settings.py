7d09497effc6da0086ac78754c00f7e720951899
BakdauletBolat <61851041+BakdauletBolat@users.noreply.github.com>
2024-07-15 14:40:25 +0500

    Update prod_settings.py

--- qr-back/qrmenu/prod_settings.py
+++ qr-back/qrmenu/prod_settings.py
@@ -4,7 +4,7 @@ import os
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+HOSTNAME = "https://easy menu.kz"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
